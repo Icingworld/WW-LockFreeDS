0015c167822ed136371163fcab7944918ee159f8
icingworld <984442022@qq.com>
2025-03-13 17:21:07 +0800

    feat: create hazard pointer manager

--- containers/ww_hazard_pointer.h
+++ containers/ww_hazard_pointer.h
@@ -4,127 +4,10 @@
 #include <memory>
 #include <thread>
 #include <functional>
+#include <array>
 
 namespace wwlockfree
 {
-
-class hazard_pointer;
-
-// 风险指针列表
-
-static constexpr std::size_t _Hazard_pointer_max = 128;             // 风险指针最大数量
-static hazard_pointer _Hazard_pointer_list[_Hazard_pointer_max];    // 风险指针列表
-
-/**
- * @brief 检查一个指针是否被风险指针保护
- * @param _Ptr 指针
- * @return 是否被风险指针保护
- */
-bool _Search_hazard_pointer(void * _Ptr)
-{
-    for (std::size_t i = 0; i < _Hazard_pointer_max; ++i) {
-        if (_Hazard_pointer_list[i]._Protect_ptr->load() == _Ptr) {
-            return true;
-        }
-    }
-    return false;
-}
-
-/**
- * @brief hazard pointer reclaim node
- * @details 用于延迟删除的链表节点
- */
-class _Relaim_node
-{
-public:
-    using _Node_pointer = _Relaim_node *;
-
-public:
-    _Node_pointer _Next;                    // 后继指针
-    void * _Del_ptr;                        // 待删除指针
-    std::function<void(void*)> _Deleter;    // 删除器
-
-public:
-    template<typename _Ty>
-    _Relaim_node(_Ty * _Ptr)
-        : _Next(nullptr)
-        , _Del_ptr(_Ptr)
-       , _Deleter([](void * _P) {
-            delete static_cast<_Ty *>(_P);
-        })
-    {
-    }
-
-    ~_Relaim_node()
-    {
-        _Deleter(_Del_ptr);
-    }
-};
-
-/**
- * @brief hazard pointer reclaim list
- * @details 用于延迟删除的链表
- */
-class _Reclaim_list
-{
-public:
-    using _Node = _Relaim_node;
-    using _Node_pointer = _Relaim_node *;
-
-public:
-    std::atomic<_Node_pointer> _Head;   // 头指针
-
-public:
-    _Reclaim_list()
-    {
-        _Head.store(nullptr);
-    }
-
-public:
-    /**
-     * @brief 插入一个节点
-     */
-    void push(const _Node_pointer & _Ptr)
-    {
-        // 直接插入到链表头部
-        _Ptr->_Next = _Head.load();
-        while (!_Head.compare_exchange_weak(_Ptr->_Next, _Ptr));
-    }
-
-    /**
-     * @brief 将一个指针插入到链表中
-     */
-    template <typename _Ty>
-    void push(_Ty * _Ptr)
-    {
-        push(new _Node(_Ptr));
-    }
-
-    /**
-     * @brief 释放所有节点
-     * @details 尝试释放所有能够释放的节点
-     * 不能释放的节点会重新被添加到链表中
-     */
-    void release() {
-        // 获取当前所有待删除的节点，并清空 _Head
-        _Node_pointer current = _Head.exchange(nullptr);
-    
-        while (current) {
-            _Node_pointer next = current->_Next;
-    
-            // 检查当前节点是否仍然被风险指针保护
-            if (!_Search_hazard_pointer(current->_Del_ptr)) {
-                delete current;  // 没有风险指针，直接删除
-            } else {
-                push(current);  // 仍然有风险指针，重新放入待删除链表
-            }
-    
-            current = next;
-        }
-    }
-};
-
-static _Reclaim_list _Reclaim_list_instance;    // 延迟删除链表
 
 /**
  * @brief hazard_pointer_obj_base
@@ -166,7 +49,7 @@ public:
     {
         if (_Should_delay) {
             // 延迟删除，添加到待删除链表中
-            _Reclaim_list_instance.push(static_cast<_Ty *>(this));
+            
         } else {
             // 立即删除
             _Deleter(static_cast<_Ty *>(this));
@@ -288,5 +171,42 @@ void swap(hazard_pointer & _Left, hazard_pointer & _Right) noexcept
 {
     _Left.swap(_Right);
 }
+
+/**
+ * @brief hazard pointer manager
+ * @details 用于管理风险指针
+ */
+class hazard_pointer_manager
+{
+private:
+    static constexpr std::size_t _Hazard_pointer_max = 128;                         // 最大风险指针数量
+    static std::array<hazard_pointer, _Hazard_pointer_max> _Hazard_pointer_list;    // 风险指针列表
+
+public:
+    hazard_pointer_manager() = default;
+
+    hazard_pointer_manager(const hazard_pointer_manager &) = delete;
+
+    hazard_pointer_manager(hazard_pointer_manager &&) = delete;
+
+    hazard_pointer_manager & operator=(const hazard_pointer_manager &) = delete;
+
+    hazard_pointer_manager & operator=(hazard_pointer_manager &&) = delete;
+
+    ~hazard_pointer_manager() = default;
+
+public:
+    /**
+     * @brief 获取一个风险指针
+     * @return 风险指针
+     */
+    static hazard_pointer * get_hazard_pointer() noexcept
+    {
+        
+    }
+};
+
+// 初始化静态变量
+std::array<hazard_pointer, hazard_pointer_manager::_Hazard_pointer_max> hazard_pointer_manager::_Hazard_pointer_list;
 
 } // namespace wwlockfree
